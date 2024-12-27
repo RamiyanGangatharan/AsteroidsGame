fa0e8a71e13d9a548aa164494043f130a6d54357
RamiyanGangatharan <135138789+RamiyanGangatharan@users.noreply.github.com>
2024-12-27 14:34:39 -0500

    Main Menu -Broken

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button PlayButton;
+    public Button ExitButton;
+
+    void Start()
+    {
+        PlayButton.onClick.AddListener(PlayGame);
+    }
+    void Update()
+    {
+        
+    }
+
+    private void PlayGame()
+    {
+        Debug.Log("Play Button has been clicked.");
+        SceneManager.LoadScene(1);
+    }
+}
